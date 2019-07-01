f0c605d6a807b6789d51f0ca6ffda21f902a288b
Chad Fennell <libsys@gmail.com>
2019-07-01 11:41:02 -0500

    rebuild test as well as dev versions

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker image rmi -f umedia_solr:dev; docker build . --tag umedia_solr:dev --no-cache
+docker image rmi -f umedia_solr:test; docker build . --tag umedia_solr:test --no-cache
