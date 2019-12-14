3e9c2cf93a9d58afc01b0efe91f1578dce2d58f5
Chad Fennell <libsys@gmail.com>
2019-12-14 10:39:44 -0600

    always build locally; it's faster to import a container, but building locally is more secure

--- rebuild-test.sh
+++ rebuild-test.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker image rmi -f umnlib/umedia_solr:test; docker build . --tag umnlib/umedia_solr:test --no-cache
-docker push umnlib/umedia_solr:test
+docker image rmi -f umedia_solr:test; docker build . --tag umedia_solr:test --no-cache
