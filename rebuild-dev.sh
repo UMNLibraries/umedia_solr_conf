32710a60077a9c6d788580d6911a888af63711df
Michael Berkowski <mjb@umn.edu>
2021-11-04 09:09:30 -0500

    Consolidate image build in one script

--- rebuild-dev.sh
+++ rebuild-dev.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-docker image rmi -f umedia_solr:dev; docker build . --tag umedia_solr:dev --no-cache
+#
+# USAGE ./rebuild.sh dev|test
+# Deletes old Docker images and rebuilds for TAG dev|test
+tag="$1"
+docker image rmi -f "umedia_solr:${tag}"; docker build . --tag "umedia_solr:${tag}" --no-cache
